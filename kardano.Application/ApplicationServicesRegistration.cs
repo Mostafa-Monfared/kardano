fba1def3887e22be3bb118293d0be5e76d9a6428
Mostafa Monfared <mostafa.es9919@gmail.com>
2022-04-26 16:32:45 +0430

    first init

---
+++ kardano.Application/ApplicationServicesRegistration.cs
@@ -0,0 +1,13 @@
+﻿namespace kardano.Application
+{
+    public static class ApplicationServicesRegistration
+    {
+        public static IServiceCollection ConfigureApplicationServices(this IServiceCollection services)
+        {
+            services.AddAutoMapper(Assembly.GetExecutingAssembly());
+            services.AddMediatR(Assembly.GetExecutingAssembly());
+
+            return services;
+        }
+    }
+}
